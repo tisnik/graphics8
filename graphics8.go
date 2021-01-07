e26dc706ff34c726d886d27aa59fb88a9af9731a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-07 13:28:23 +0100

    Server

--- graphics8.go
+++ graphics8.go
@@ -12,5 +12,43 @@
 
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func handler(connection net.Conn) {
+	for {
+		fmt.Fprintf(connection, "?\n")
+		status, err := bufio.NewReader(connection).ReadString('\n')
+		if err != nil {
+			log.Print("No response!")
+			return
+		} else {
+			log.Print(status)
+		}
+	}
+}
+
+func server(listener net.Listener) {
+	log.Print("Waiting for connections")
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Print("Connection refused!")
+		}
+		defer connection.Close()
+		go handler(connection)
+	}
+}
+
 func main() {
+	l, err := net.Listen("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("Can't open the port!", err)
+	}
+	defer l.Close()
+	server(l)
 }
